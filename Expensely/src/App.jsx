d2c289e1664f651503a11682ae615bf596dc8412
ASHUTOSH VISHWAKARMA <ashutoshvishwakarma208@outlook.com>
2024-05-23 22:24:45 +0530

    first commit

---
+++ Expensely/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import { Routes, Route } from "react-router-dom";
+import { Home } from './components';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home/>} />
+    </Routes>
+  )
+}
+
+export default App
